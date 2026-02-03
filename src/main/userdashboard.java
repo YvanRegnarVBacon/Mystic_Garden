6612548e2884b4fd40aeaba9992193c8a06be2dd
YvanRegnarVBacon <ivanbacon2001@gmail.com>
2026-02-04 02:06:58 +0800

    NOTFINISH

--- src/main/userdashboard.java
+++ src/main/userdashboard.java
@@ -4,19 +4,32 @@
  * and open the template in the editor.
  */
 package main;
-
+import java.awt.Font;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import javax.swing.SwingWorker;
+import java.sql.PreparedStatement;
 /**
  *
  * @author 63936
  */
 public class userdashboard extends javax.swing.JFrame {
+    private int loggedInUserId;
 
     /**
      * Creates new form userdashboard
      */
-    public userdashboard() {
+    public userdashboard(int userId) {
         initComponents();
-        
+        this.loggedInUserId = userId;
+        loadUserData();
+    }
+
+    userdashboard() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     /**
@@ -29,33 +42,31 @@ public class userdashboard extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        User_Name = new javax.swing.JTextField();
-        User_ID = new javax.swing.JTextField();
-        User_Status = new javax.swing.JTextField();
-        User_Email = new javax.swing.JTextField();
+        User_ID = new javax.swing.JLabel();
+        User_Gender = new javax.swing.JLabel();
+        User_Name = new javax.swing.JLabel();
+        User_Status = new javax.swing.JLabel();
+        User_Email = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel2.setBackground(new java.awt.Color(153, 153, 255));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/CaptureNEW.PNG"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1042, 290));
-
         jPanel1.setBackground(new java.awt.Color(102, 51, 255));
-        jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(102, 102, 255), new java.awt.Color(102, 102, 255), new java.awt.Color(102, 0, 255), new java.awt.Color(102, 0, 255)));
+        jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 102, 255), new java.awt.Color(102, 102, 255)));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel2.setBackground(new java.awt.Color(255, 255, 255));
@@ -63,9 +74,6 @@ public class userdashboard extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("ACCOUNT DETAILS");
         jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(101, 3, -1, 46));
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/NewICON.PNG"))); // NOI18N
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(732, 55, -1, -1));
 
         jLabel4.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
@@ -79,66 +87,88 @@ public class userdashboard extends javax.swing.JFrame {
 
         jLabel6.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setText("EMAIL:");
+        jLabel6.setText("GENDER:");
         jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(58, 198, -1, -1));
 
         jLabel7.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 255, 255));
         jLabel7.setText("USER STATUS:");
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 260, -1, -1));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 250, -1, -1));
 
-        User_Name.setBackground(new java.awt.Color(102, 51, 255));
-        User_Name.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
-        User_Name.setForeground(new java.awt.Color(255, 255, 255));
-        User_Name.setBorder(null);
-        jPanel1.add(User_Name, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 136, 200, 37));
-
-        User_ID.setBackground(new java.awt.Color(102, 51, 255));
-        User_ID.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
+        User_ID.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         User_ID.setForeground(new java.awt.Color(255, 255, 255));
-        User_ID.setBorder(null);
-        User_ID.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                User_IDActionPerformed(evt);
-            }
-        });
-        jPanel1.add(User_ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 81, 200, 37));
+        User_ID.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 0, 204), new java.awt.Color(102, 0, 204)));
+        jPanel1.add(User_ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 80, 260, 37));
 
-        User_Status.setBackground(new java.awt.Color(102, 51, 255));
-        User_Status.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
+        User_Gender.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        User_Gender.setForeground(new java.awt.Color(255, 255, 255));
+        User_Gender.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 0, 204), new java.awt.Color(102, 0, 204)));
+        jPanel1.add(User_Gender, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 190, 260, 35));
+
+        User_Name.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        User_Name.setForeground(new java.awt.Color(255, 255, 255));
+        User_Name.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 0, 204), new java.awt.Color(102, 0, 204)));
+        jPanel1.add(User_Name, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 140, 260, 35));
+
+        User_Status.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         User_Status.setForeground(new java.awt.Color(255, 255, 255));
-        User_Status.setBorder(null);
-        User_Status.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                User_StatusActionPerformed(evt);
-            }
-        });
-        jPanel1.add(User_Status, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 240, 200, 37));
+        User_Status.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 0, 204), new java.awt.Color(102, 0, 204)));
+        jPanel1.add(User_Status, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 260, 37));
 
-        User_Email.setBackground(new java.awt.Color(102, 51, 255));
-        User_Email.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
+        User_Email.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
         User_Email.setForeground(new java.awt.Color(255, 255, 255));
-        User_Email.setBorder(null);
-        User_Email.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                User_EmailActionPerformed(evt);
-            }
-        });
-        jPanel1.add(User_Email, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 191, 200, 37));
+        User_Email.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 0, 204), new java.awt.Color(102, 0, 204)));
+        jPanel1.add(User_Email, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 250, 230, 40));
 
-        jPanel2.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 300, 1022, -1));
+        jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/NewICON.PNG"))); // NOI18N
+        jLabel9.setText("jLabel9");
+        jLabel9.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 255), new java.awt.Color(153, 153, 255), new java.awt.Color(102, 0, 204), new java.awt.Color(102, 0, 204)));
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 10, 270, 280));
 
+        jPanel2.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, 1022, 300));
+
+        jButton1.setBackground(new java.awt.Color(153, 153, 255));
+        jButton1.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 204, 51));
         jButton1.setText("Edit Profile");
         jButton1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 610, 178, 66));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 520, 178, 66));
 
+        jButton2.setBackground(new java.awt.Color(153, 153, 255));
+        jButton2.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 204, 51));
         jButton2.setText("Order History");
         jButton2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 610, 178, 66));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 520, 178, 66));
 
+        jButton3.setBackground(new java.awt.Color(153, 153, 255));
+        jButton3.setFont(new java.awt.Font("Times New Roman", 3, 18)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 204, 51));
         jButton3.setText("Log Out");
         jButton3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 610, 178, 67));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 520, 178, 67));
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/NewUser.png"))); // NOI18N
+        jPanel2.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1070, -1));
+
+        jLabel1.setBackground(new java.awt.Color(255, 102, 102));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/aifbniajbncioaebhfiuqaiocqane.png"))); // NOI18N
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 610, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -148,23 +178,23 @@ public class userdashboard extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 715, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void User_IDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_User_IDActionPerformed
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_User_IDActionPerformed
+    }//GEN-LAST:event_jButton3ActionPerformed
 
-    private void User_StatusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_User_StatusActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_User_StatusActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void User_EmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_User_EmailActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_User_EmailActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -196,27 +226,82 @@ public class userdashboard extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new userdashboard().setVisible(true);
+                int userId = rs.getInt("u_id");
+                new userdashboard(userId).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField User_Email;
-    private javax.swing.JTextField User_ID;
-    private javax.swing.JTextField User_Name;
-    private javax.swing.JTextField User_Status;
+    private javax.swing.JLabel User_Email;
+    private javax.swing.JLabel User_Gender;
+    private javax.swing.JLabel User_ID;
+    private javax.swing.JLabel User_Name;
+    private javax.swing.JLabel User_Status;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
+
+    private void loadUserData() {
+
+    SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+
+        String id = "";
+        String name = "";
+        String gender = "";
+        String status = "";
+        String email = "";
+
+        @Override
+        protected Void doInBackground() {
+
+            String dbPath = "jdbc:sqlite:garden.db";
+            String sql = "SELECT u_id, name, email, gender, status " +
+                         "FROM user_tbl WHERE u_id = ?";
+
+            try (Connection conn = DriverManager.getConnection(dbPath);
+                 PreparedStatement pst = conn.prepareStatement(sql)) {
+
+                pst.setInt(1, loggedInUserId);
+                ResultSet rs = pst.executeQuery();
+
+                if (rs.next()) {
+                    id = rs.getString("u_id");
+                    name = rs.getString("name");
+                    gender = rs.getString("gender");
+                    status = rs.getString("status");
+                    email = rs.getString("email");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void done() {
+            User_ID.setText(id);
+            User_Name.setText(name);
+            User_Gender.setText(gender);
+            User_Status.setText(status);
+            User_Email.setText(email);
+        }
+    };
+
+    worker.execute();
+}
+
 }
